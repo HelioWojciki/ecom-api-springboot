b3d6763546f14c05cf810e952d0f7d4f28b0e627
Helio <helio_wojciki@hotmail.com>
2025-10-07 00:08:35 -0300

    Project created

---
+++ src/main/java/com/helio/ecom_api_springboot/EcomApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.helio.ecom_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helio/ecom_api_springboot/EcomApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helio.ecom_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
