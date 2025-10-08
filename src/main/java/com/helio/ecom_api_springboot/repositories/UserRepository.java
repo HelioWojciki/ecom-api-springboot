2ba0c0cdba944b80335c1efab79c530f5578ea6b
Helio <helio_wojciki@hotmail.com>
2025-10-08 00:07:20 -0300

    Service Layer, component registration

--- src/main/java/com/helio/ecom_api_springboot/repositories/UserRepository.java
+++ src/main/java/com/helio/ecom_api_springboot/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.helio.ecom_api_springboot.repositories;
 
 import com.helio.ecom_api_springboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
 
 }
--- src/main/java/com/helio/ecom_api_springboot/resources/UserResource.java
+++ src/main/java/com/helio/ecom_api_springboot/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.helio.ecom_api_springboot.resources;
 
 import com.helio.ecom_api_springboot.entities.User;
+import com.helio.ecom_api_springboot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "José", "Jose@gmail.com","42991229998" , "tigre");
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(u);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
