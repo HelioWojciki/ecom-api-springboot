b843c836751f224f6641214e58570ca527408f0f
Helio <helio_wojciki@hotmail.com>
2025-10-09 17:15:51 -0300

    Payment, one-to-one association

--- src/main/java/com/helio/ecom_api_springboot/config/TestConfig.java
+++ src/main/java/com/helio/ecom_api_springboot/config/TestConfig.java
@@ -31,6 +31,7 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -70,7 +71,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-    }
 
+        Payment pay1 = new Payment(null, Instant.parse("2024-09-08T22:10:00Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+    }
 
 }
