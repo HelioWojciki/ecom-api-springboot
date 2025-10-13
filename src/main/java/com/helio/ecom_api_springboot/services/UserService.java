77056abc7f6ddc27a4f4e4bf1f879aabf14849c4
Helio <helio_wojciki@hotmail.com>
2025-10-12 21:12:41 -0300

    Exception handling - update

--- src/main/java/com/helio/ecom_api_springboot/services/UserService.java
+++ src/main/java/com/helio/ecom_api_springboot/services/UserService.java
@@ -4,9 +4,9 @@ import com.helio.ecom_api_springboot.entities.User;
 import com.helio.ecom_api_springboot.repositories.UserRepository;
 import com.helio.ecom_api_springboot.services.exceptions.DatabaseException;
 import com.helio.ecom_api_springboot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,9 +43,13 @@ public class UserService {
      }
 
      public User update(Long id, User obj){
-         User entity = userRepository.getReferenceById(id); // prepara o obj
-         updateData(entity, obj);
-         return userRepository.save(entity);
+         try {
+             User entity = userRepository.getReferenceById(id); // prepara o obj
+             updateData(entity, obj);
+             return userRepository.save(entity);
+         } catch (EntityNotFoundException e){
+             throw new ResourceNotFoundException(id);
+         }
      }
 
     private void updateData(User entity, User obj) {
