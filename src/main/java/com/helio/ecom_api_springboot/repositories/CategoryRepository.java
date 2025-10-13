913e8ec51ec5becdc36a638b776f4893cdf65a10
Helio <helio_wojciki@hotmail.com>
2025-10-13 02:50:03 -0300

    Dev profile

--- src/main/java/com/helio/ecom_api_springboot/repositories/CategoryRepository.java
+++ src/main/java/com/helio/ecom_api_springboot/repositories/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.helio.ecom_api_springboot.repositories;
 import com.helio.ecom_api_springboot.entities.Category;
 import com.helio.ecom_api_springboot.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/helio/ecom_api_springboot/repositories/OrderRepository.java
+++ src/main/java/com/helio/ecom_api_springboot/repositories/OrderRepository.java
@@ -2,7 +2,9 @@ package com.helio.ecom_api_springboot.repositories;
 
 import com.helio.ecom_api_springboot.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
