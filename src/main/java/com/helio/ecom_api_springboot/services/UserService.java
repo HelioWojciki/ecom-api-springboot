d0cc9a0ef90ed24b25b5ef4c5dffdbc4e299b18a
Helio <helio_wojciki@hotmail.com>
2025-10-14 01:52:23 -0300

    fix: Ensure transactionality for CRUD operations using @Transactional in users

--- src/main/java/com/helio/ecom_api_springboot/services/UserService.java
+++ src/main/java/com/helio/ecom_api_springboot/services/UserService.java
@@ -8,6 +8,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,40 +19,46 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-     public List<User> findAll (){
-         return userRepository.findAll();
-     }
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
-     public User findById (Long id){
-         Optional<User> obj = userRepository.findById(id);
-         return obj.orElseThrow(() -> new ResourceNotFoundException(id)) ;
-     }
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
 
-     public User insert(User obj){
-         return userRepository.save(obj); // direto, pois já retorna o obj salvo
-     }
+    @Transactional
+    public User insert(User obj) {
+        return userRepository.save(obj); // direto, pois já retorna o obj salvo
+    }
 
-     public void delete(Long id){
-         if (!userRepository.existsById(id)){
-             throw new ResourceNotFoundException(id);
-         }
-         try {
-             userRepository.deleteById(id);
-         } catch (DataIntegrityViolationException e) {
-             throw new DatabaseException(e.getMessage());
-         }
-     }
+    @Transactional
+    public void delete(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            userRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 
-     public User update(Long id, User obj){
-         try {
-             User entity = userRepository.getReferenceById(id); // prepara o obj
-             updateData(entity, obj);
-             return userRepository.save(entity);
-         } catch (EntityNotFoundException e){
-             throw new ResourceNotFoundException(id);
-         }
-     }
+    @Transactional
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id); // prepara o obj
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
 
+    @Transactional
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
